4a5139417b2a25995f625de34d389a756bb5033c
Dang Khoa Tran <dangkhoa200299@gmail.com>
2023-10-14 00:37:02 +0700

    Chessboard Design

---
+++ MyChinesChess/Models/MoveChess.cs
@@ -0,0 +1,11 @@
+﻿namespace MyWebChess.Models;
+
+public class MoveChess
+{
+    public string id { get; set; }
+
+    public int fromi { get; set; }
+    public int fromj { get; set; }
+    public int toi { get; set; }
+    public int toj { get; set; }
+}
---
+++ MyChinesChess/Models/PointModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyWebChess.Models
+{
+    public class PointModel
+    {
+        public int top { get; set; }
+        public int left { get; set; }
+        public string id { get; set; }
+    }
+}

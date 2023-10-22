04657ebcf2e1dc41844fbed6c11dd95846b4627a
hongha12345 <karagirikazata@gmail.com>
2023-10-22 17:14:00 +0700

    dangnhap-dangky-jwt

---
+++ Libs/ApplicationDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Libs.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Libs
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Room> Room { get; set; }
+        public DbSet<UserInRoom> UserInRoom { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER",
+                Id = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+
+            });
+
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN",
+                Id = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+
+            });
+        }
+    }
+}
